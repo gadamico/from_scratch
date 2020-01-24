e9ecc5ddae5b37df75b640a1c122eaad2b9ab0a6
Greg Damico <greg.damico@flatironschool.com>
2020-01-23 16:04:04 -0800

    fix plot code

--- rand_scatter.py
+++ rand_scatter.py
@@ -5,10 +5,10 @@ def scatter():
     Y = np.random.rand(100, 1)
     fig, ax = plt.subplots(1, 3)
     ax[0].hist(X)
-    ax[0].title('X')
+    ax[0].title.set_text('X')
     ax[1].hist(Y)
-    ax[1].title('Y')
+    ax[1].title.set_text('Y')
     ax[2].scatter(X, Y)
-    ax[2].title('X Vs. Y')
+    ax[2].title.set_text('X Vs. Y')
     plt.show()
 
